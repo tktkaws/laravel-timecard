700d2424e783ecf690725c9e38b8d5e23ee840f6
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-08-12 08:59:44 +0900

    create index_view

---
+++ app/Http/Controllers/StartTimeController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StartTimeController extends Controller
+{
+    //==========ここから追加==========
+    public function index()
+    {
+        // ダミーデータ
+        $start_times = [
+            (object) [
+                'id' => 1,
+                'title' => 'タイトル1',
+                'body' => '本文1',
+                'created_at' => now(),
+                'user' => (object) [
+                    'id' => 1,
+                    'name' => 'ユーザー名1',
+                ],
+            ],
+            (object) [
+                'id' => 2,
+                'title' => 'タイトル2',
+                'body' => '本文2',
+                'created_at' => now(),
+                'user' => (object) [
+                    'id' => 2,
+                    'name' => 'ユーザー名2',
+                ],
+            ],
+            (object) [
+                'id' => 3,
+                'title' => 'タイトル3',
+                'body' => '本文3',
+                'created_at' => now(),
+                'user' => (object) [
+                    'id' => 3,
+                    'name' => 'ユーザー名3',
+                ],
+            ],
+        ];
+
+        return view('start_times.index', ['start_times' => $start_times]);
+    }
+}
